670c6384160a8b4db4bd9fef8c2cc59370160161
Esha Himagirish <128863682+eshahima@users.noreply.github.com>
2024-12-17 13:59:36 -0600

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,44 @@
+
+name ="Esha"
+#1string are anything encapsulated with double quotes
+
+age = 17
+#2int is whole number
+
+decimal = 13.89
+#3floating is a decimal number
+
+driverslicense = True
+#4boolean true or false
+
+hobbies = {'dancing', 'coding'}
+#5set is unordered collection that has no duplicate elements
+
+classes = {1: 'ap chem', 2: 'ap calc bc', 3: 'ap lit'}
+#6dictionary stores in format of key: value
+
+likes = ["pink", "ap stats"]
+#7lists dynamically sized arrays;
+
+number = (1, 13, 14)
+#8tuple contains any type and indexed by integers
+
+print("tell me something")
+print(input())
+
+print("enter three numbers")
+var1 = input()
+
+print("input second number")
+var2 = input()
+
+print("input third number")
+var3 = input()
+
+print("here's the output: " )
+var4= int(var1) *int(var2) *int(var3)
+print(var4)
+
+print("input something you like to do: ")
+thing = input()
+print("Hi! I like to " +thing + ".")
